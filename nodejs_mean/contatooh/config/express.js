8d0637fe413cb7a7a514ae1c6a4d78b7ecc3fb1d
Fernando da Silva Rosa <fernandosr@ciandt.com>
2016-01-20 16:44:53 -0200

    terminio da primeira parte do back end

--- nodejs_mean/contatooh/config/express.js
+++ nodejs_mean/contatooh/config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var home = require('../app/routes/home');
+var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function(){
   var app = express();
@@ -11,7 +12,14 @@ module.exports = function(){
   app.set('view engine', 'ejs');
   app.set('views','./app/views');
 
-  home(app);
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(require('method-override')());
+
+  load('models', {cwd: 'app'})
+  .then('controllers')
+  .then('routes')
+  .into(app);
 
   return app;
 };
